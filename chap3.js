ec24060e17a1c9d2a5c39d784f632c60c519f5cc
unknown <cmrg.vinicius@gmail.com>
2015-03-30 22:12:30 -0300

    start list class implementation

---
+++ chap3.js
@@ -0,0 +1,21 @@
+// A list class implementation
+function List() {
+	this.listSize = 0;
+	this.pos = 0;
+	this.dataStore = [];
+	this.clear = clear;
+	this.find = find;
+	this.toString = toString;
+	this.insert = insert;
+	this.append = append;
+	this.remove = remove;
+	this.front = front;
+	this.end = end;
+	this.prev = prev;
+	this.next = next;
+	this.length = length;
+	this.currPos = currPos;
+	this.moveTo = moveTo;
+	this.getElement = getElement;
+	this.contains = contains;
+}
