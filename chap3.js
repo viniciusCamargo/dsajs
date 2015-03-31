551dbb3822057d664963c5509d4774f380a38ffd
unknown <cmrg.vinicius@gmail.com>
2015-03-30 23:03:46 -0300

    create all the methods for the list

--- chap3.js
+++ chap3.js
@@ -93,4 +93,35 @@ function contains(element) {
 	return false;
 }
 
+// Traversing a list
+function front() {
+	this.pos = 0;
+}
 
+function end() {
+	this.pos = this.listSize-1;
+}
+
+function prev() {
+	if (this.pos > 0) {
+		--this.pos;
+	}
+}
+
+function next() {
+	if (this.pos < this.listSize-1) {
+		++this.pos;
+	}
+}
+
+function currPos() {
+	return this.pos;
+}
+
+function moveTo(position) {
+	this.pos = position;
+}
+
+function getElement() {
+	return this.dataStore[this.pos];
+}
