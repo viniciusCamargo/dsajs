1bfdedb454591460185bcdea8fa1676eea6e4e39
unknown <cmrg.vinicius@gmail.com>
2015-03-30 22:36:17 -0300

    comment the undefined methods of the list implementation

--- chap3.js
+++ chap3.js
@@ -3,21 +3,21 @@ function List() {
 	this.listSize = 0;
 	this.pos = 0;
 	this.dataStore = [];
-	this.clear = clear;
+	// this.clear = clear;
 	this.find = find;
 	this.toString = toString;
-	this.insert = insert;
+	// this.insert = insert;
 	this.append = append;
 	this.remove = remove;
-	this.front = front;
-	this.end = end;
-	this.prev = prev;
-	this.next = next;
+	// this.front = front;
+	// this.end = end;
+	// this.prev = prev;
+	// this.next = next;
 	this.length = length;
-	this.currPos = currPos;
-	this.moveTo = moveTo;
-	this.getElement = getElement;
-	this.contains = contains;
+	// this.currPos = currPos;
+	// this.moveTo = moveTo;
+	// this.getElement = getElement;
+	// this.contains = contains;
 }
 
 // Append: adding an element to a list
@@ -55,3 +55,4 @@ function length() {
 function toString() {
 	return this.dataStore;
 }
+
